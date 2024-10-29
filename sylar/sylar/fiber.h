0506219a3aa5bb9e81fcded9ca5e73131b4321ba
cnzed <cnzed99@163.com>
2024-10-29 03:35:09 -0700

    基础协程模块完成70%

--- sylar/sylar/fiber.h
+++ sylar/sylar/fiber.h
@@ -1,43 +1,71 @@
-#pragma once
-//协程调度器封装
+#ifndef __SYLAR_FIBER_H__
+#define __SYLAR_FIBER_H__
 
 #include <memory>
 #include <functional>
 #include <ucontext.h>
+#include "thread.h"
 
-namespace sylar
-{
-    class Scheduler;
+namespace sylar {
 
-    /**
-     * @brief 协程类
-     */
-    class Fiber : public std::enable_shared_from_this<Fiber> 
-    {
-        friend class Scheduler;
-        public:
-        typedef std::shared_ptr<Fiber> ptr;
+class Scheduler;
+class Fiber : public std::enable_shared_from_this<Fiber> {
+friend class Scheduler;
+public:
+    typedef std::shared_ptr<Fiber> ptr;
 
-        //协程状态
-        enum State
-        {
-            INIT,   //初始化状态
-            HOLD,   //暂停状态
-            EXEC,   //执行中状态
-            TERM,   //结束状态
-            READY,  //可执行状态
-            EXCEPT  //异常状态
-        };
-
-        private:
-        //每个线程第一个协程的构造，无参构造
-        Fiber();
-
-        public:
-        Fiber(std::function<void()> cb, size_t stacksize = 0, bool use_caller = false);
-
-        ~Fiber();
-
-        
+    enum State {
+        INIT,
+        HOLD,
+        EXEC,
+        TERM,
+        READY,
+        EXCEPT
     };
+private:
+    Fiber();
+
+public:
+    Fiber(std::function<void()> cb, size_t stacksize = 0);
+    ~Fiber();
+
+    //重置协程函数，并重置状态
+    //INIT，TERM
+    void reset(std::function<void()> cb);
+    //切换到当前协程执行
+    void swapIn();
+    //切换到后台执行
+    void swapOut();
+
+    void call();
+
+    uint64_t getId() const { return m_id;}
+    State getState() const { return m_state;}
+public:
+    //设置当前协程
+    static void SetThis(Fiber* f);
+    //返回当前协程
+    static Fiber::ptr GetThis();
+    //协程切换到后台，并且设置为Ready状态
+    static void YieldToReady();
+    //协程切换到后台，并且设置为Hold状态
+    static void YieldToHold();
+    //总协程数
+    static uint64_t TotalFibers();
+
+    static void MainFunc();
+    static uint64_t GetFiberId();
+private:
+    uint64_t m_id = 0;
+    uint32_t m_stacksize = 0;
+    State m_state = INIT;
+
+    ucontext_t m_ctx;
+    void* m_stack = nullptr;
+
+    std::function<void()> m_cb;
+};
+
 }
+
+#endif
